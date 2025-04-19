920e6e6bd26592fe90328b96b94e6ae4e9d090a5
tetiana_hushnovska <thushnyovska@gmail.com>
2025-04-19 22:20:46 +0100

    remove propTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 
 function Profile({ name, tag, location, image, stats }) {
@@ -28,19 +27,5 @@ function Profile({ name, tag, location, image, stats }) {
     </div>
   );
 }
-
-Profile.propTypes = {
-  userData: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    stats: PropTypes.shape({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
-    }).isRequired,
-  }).isRequired,
-};
 
 export default Profile;
